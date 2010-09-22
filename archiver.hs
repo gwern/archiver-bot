a9bb4ec1d9def4cc4a67aa16173e3024ccdde150
gwern0 <gwern0@gmail.com>
2010-09-22 13:23:52 -0400

    first try at hinotify
    Ignore-this: beb0ead15a875872431d9d0e59b2a1fe
    
    darcs-hash:20100922172352-f7719-9eb56aab058c588a99ebe3af82ab87c6a958984f

--- archiver.hs
+++ archiver.hs
@@ -7,26 +7,31 @@ import Network.HTTP (getRequest, rspBody, simpleHTTP, RequestMethod(POST))
 import Network.URI (isURI, parseURI, uriPath)
 import System.Environment
 
+import System.INotify
+
 main :: IO ()
 main = do args <- getArgs
           case args of
-           (f:[]) -> archivePage Nothing f
-           (f:e:[]) -> archivePage (Just e) f
+           (f:[]) ->   watch Nothing f
+           (f:e:[]) -> watch (Just e) f
            _ -> error "must supply a filename or a filename and an email adress"
-          
+
+watch :: Maybe String -> FilePath -> IO ()
+watch user file = do i <- initINotify
+                     _ <- addWatch i [AllEvents] file (\_ -> archivePage user file)
+                     return ()
+
 archivePage :: Maybe String -> FilePath -> IO ()
-archivePage user file = loop
-                    where
-                        email = fromMaybe "nobody@mailinator.com" user
-                        loop = do contents <- B.readFile file
-                                  let (url,rest) = B.break (=='\n') contents
-                                  checkArchive email (B.unpack url)
-                                  print url
-                                  -- banned >=100 requests/hour; choke to ~1/minute
-                                  threadDelay 40000000 -- ~40 seconds
-                                  if B.length rest /= 0 then do B.writeFile file (B.drop 1 rest) -- drop to get rid of leading \n
-                                                                loop
-                                   else return ()
+archivePage user file = do contents <- B.readFile file
+                           let (url,rest) = B.break (=='\n') contents
+                           checkArchive email (B.unpack url)
+                           print url
+                           -- banned >=100 requests/hour; choke to ~1/minute
+                           threadDelay 40000000 -- ~40 seconds
+                           when (B.length rest /= 0) $ (B.writeFile file (B.drop 1 rest) >> archivePage user file) -- drop to get rid of leading \n
+                           return ()
+                           where
+                               email = fromMaybe "nobody@mailinator.com" user
 
 -- | Error check the URL and then archive it both ways
 checkArchive :: String -> String -> IO ()
