c3eecfe8b068b6f6ec50b27b8107ac7df3b353a1
gwern0 <gwern0@gmail.com>
2010-09-23 11:25:25 -0400

    fmt docs
    Ignore-this: ad0d117897b0a9ab8727d651598871da
    
    darcs-hash:20100923152525-f7719-aef7c0489236cb3b6b48e6702623c7aeda9ed616

--- Network/URL/Archiver.hs
+++ Network/URL/Archiver.hs
@@ -8,20 +8,23 @@ import Network.URI (isURI, parseURI, uriPath)
 
 
 -- | Error check the URL and then archive it using 'webciteArchive' and 'alexaArchive'
-checkArchive :: String -> String -> IO ()
+checkArchive :: String -- ^ email for WebCite to send status to 
+                -> String -- ^ URL to archive
+                -> IO ()
 checkArchive email url = when (isURI url) (webciteArchive email url >> alexaArchive url)
 
-{- | Request <www.webcitation.org> to copy a supplied URL; WebCite does on-demand archiving, unlike Alexa/Internet Archive,
-   and so in practice this is the most useful function.
+{- | Request <http://www.webcitation.org> to copy a supplied URL; WebCite does on-demand archiving, unlike Alexa/Internet Archive,
+   and so in practice this is the most useful function. This function throws away any return status from WebCite (which may be changed
+   in the future), so it is suggested that one test with a valid email address.
  
-   *Warning!* WebCite has throttling mechanisms; if you request more than 100 URLs per hour, your IP may be banned! It is
+   /Warning!/ WebCite has throttling mechanisms; if you request more than 100 URLs per hour, your IP may be banned! It is
    suggested that one sleep for \~40 seconds between each URL request. -}
 webciteArchive :: String -> String -> IO ()
 webciteArchive email url = openURL ("http://www.webcitation.org/archive?url=" ++ url ++ "&email=" ++ email)
                            >> return ()
    where openURL = simpleHTTP . getRequest
 
--- | Request <www.alexa.com> to spider a supplied URL; Alexa supplies the Internet Archive's caches (<www.archive.org>).
+-- |  Request <http://www.alexa.com> to spider a supplied URL. Alexa supplies the Internet Archive's caches.
 alexaArchive :: String -> IO ()
 alexaArchive url = do let archiveform = Form POST
                              (fromJust $ parseURI "http://www.alexa.com/help/crawlrequest")
