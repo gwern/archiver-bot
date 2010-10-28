c6096529c16a2e308fadf79efe687c1d34828b7c
gwern0 <gwern0@gmail.com>
2010-10-27 21:27:56 -0400

    archiver.hs: test for network connectivity
    Ignore-this: 94404d36eacbc0bc9358182d77a2a5bf
    
    darcs-hash:20101028012756-f7719-482191a8273462427328421cb94eb4262852d06d

--- archiver.hs
+++ archiver.hs
@@ -2,6 +2,7 @@ import Control.Concurrent (threadDelay)
 import Control.Monad (forever, liftM, when)
 import Data.List (nub, sort)
 import Data.Maybe (fromMaybe)
+import Network.HTTP (getRequest, simpleHTTP)
 import System.Environment (getArgs)
 import qualified Data.ByteString.Char8 as B (break, concat, length, readFile, split, unpack, writeFile, ByteString)
 
@@ -17,20 +18,25 @@ main = do args <- getArgs
            _ -> error "must supply a filename or a filename and an email address"
 
 watch :: Maybe String -> FilePath -> IO ()
-watch user file = do i <- initINotify
-                     archivePage user file -- empty out existing file, then we add a watch & sleep
-                     _ <- addWatch i [AllEvents] file (\_ -> archivePage user file)
-                     forever $ threadDelay maxBound -- sleep... forever
+watch usr file = do i <- initINotify
+                    archivePage usr file -- empty out existing file, then we add a watch & sleep
+                    _ <- addWatch i [AllEvents] file (\_ -> archivePage usr file)
+                    forever $ threadDelay maxBound -- sleep... forever. inotify will wake us up
 
 archivePage :: Maybe String -> FilePath -> IO ()
-archivePage user file = do contents <- B.readFile file
-                           let (url,rest) = B.break (=='\n') contents
-                           checkArchive email (B.unpack url)
-                           print url
-                           -- banned >=100 requests/hour; choke to ~1/minute
-                           threadDelay 40000000 -- ~40 seconds
-                           when (B.length rest /= 0) (writePages file url >> archivePage user file) -- drop to get rid of leading \n
-                               where email = fromMaybe "nobody@mailinator.com" user
+archivePage usr file = do connectedp <- simpleHTTP (getRequest "http://www.google.com/")
+                          case connectedp of
+                             Left _  -> -- Left = ConnError, network not working! sleep for a minute and try again later
+                                        threadDelay 90000000 >> archivePage usr file
+                             Right _ -> do -- we have access to the WWW, it seems. proceeding with mission!
+                                          contents <- B.readFile file
+                                          let (url,rest) = B.break (=='\n') contents
+                                          checkArchive email (B.unpack url)
+                                          print url
+                                          -- banned >=100 requests/hour; choke to ~1/minute
+                                          threadDelay 40000000 -- ~40 seconds
+                                          when (B.length rest /= 0) (writePages file url >> archivePage usr file) -- drop to get rid of leading \n
+                                              where email = fromMaybe "nobody@mailinator.com" usr
 
 -- re-reads a possibly modified 'file' from disk, removes the archived URL from it, and writes it back out for 'archivePage' to read immediately
 writePages :: FilePath -> B.ByteString -> IO ()
