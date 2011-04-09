873130e77a3545f48c9c7dbaf29af0aed82a8d31
gwern0 <gwern0@gmail.com>
2011-04-09 18:37:15 -0400

    archiver.cabal: ln to additional documentation
    Ignore-this: 5f3cd529e45ae057634c82b1956aba42
    
    darcs-hash:20110409223715-f7719-7f4472184436f284db1712fbea943f1f4c4b72be

--- Network/URL/Archiver.hs
+++ Network/URL/Archiver.hs
@@ -8,7 +8,7 @@ import Network.URI (isURI, parseURI, uriPath)
 
 
 -- | Error check the URL and then archive it using 'webciteArchive' and 'alexaArchive'
-checkArchive :: String -- ^ email for WebCite to send status to 
+checkArchive :: String -- ^ email for WebCite to send status to
                 -> String -- ^ URL to archive
                 -> IO ()
 checkArchive email url = when (isURI url) (webciteArchive email url >> alexaArchive url)
@@ -16,7 +16,7 @@ checkArchive email url = when (isURI url) (webciteArchive email url >> alexaArch
 {- | Request <http://www.webcitation.org> to copy a supplied URL; WebCite does on-demand archiving, unlike Alexa/Internet Archive,
    and so in practice this is the most useful function. This function throws away any return status from WebCite (which may be changed
    in the future), so it is suggested that one test with a valid email address.
- 
+
    /Warning!/ WebCite has throttling mechanisms; if you request more than 100 URLs per hour, your IP may be banned! It is
    suggested that one sleep for \~30 seconds between each URL request. -}
 webciteArchive :: String -> String -> IO ()
