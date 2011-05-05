34158df242c4383747567c41983ac2db1263c485
gwern0 <gwern0@gmail.com>
2011-05-04 20:45:12 -0400

    archiver.hs: switch away from nub & sort to the usual Data.Set hack
    Ignore-this: 7c6e1f3255e842eb20ee7bdbba8754b5
    the length of the file input recently jumped to 44.5k URLs, at which
    point the List performance of `nub . sort` starts to become a problem -
    wasting extra time and using a lot of CPU-time/battery
    
    darcs-hash:20110505004512-f7719-a0094e683d266e8ca2f9e7fbc89f87b4b4ea0631

--- archiver.hs
+++ archiver.hs
@@ -2,7 +2,8 @@
 import Control.Concurrent (threadDelay)
 import qualified Control.Exception as CE (catch, IOException)
 import Control.Monad (liftM, when)
-import Data.List (delete, nub, sort)
+import Data.List (delete)
+import qualified Data.Set as S (fromList, toList)
 import Data.Maybe (fromMaybe)
 import Network.HTTP (getRequest, simpleHTTP)
 import Network.URI (isURI)
@@ -38,7 +39,7 @@ archivePage usr file = do connectedp <- CE.catch (simpleHTTP (getRequest "http:/
 -- re-reads a possibly modified 'file' from disk, removes the archived URL from it, and writes it back out for 'archivePage' to read immediately
 writePages :: FilePath -> B.ByteString -> IO ()
 writePages file done = do original <- liftM (B.split '\n') $ B.readFile file
-                          let new = nub $ sort original
+                          let new =  S.toList $ S.fromList original
                           let final = B.intercalate (B.singleton '\n') $ filter (not . (== done)) new
                           B.writeFile file final
 
