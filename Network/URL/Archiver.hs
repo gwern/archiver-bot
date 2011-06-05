d93ede9c03b63d3d3cb17a1b58ea7ba41380d61f
gwern0 <gwern0@gmail.com>
2011-06-05 14:26:01 -0400

    Archiver.hs: filter each archive's URLs
    Ignore-this: f08f6d32694e00b8e6a9e9960dd6728f
    There is no point in asking Archive.org to archive an Archive.org address,
    or asking WebCite to archive a webcitation.org page; it's wasteful.
    (It makes sense to cross-archive, though, and that's permitted.)
    
    darcs-hash:20110605182601-f7719-08ae1b6cc591c9aba6571beb4cb547109f718fb5

--- Network/URL/Archiver.hs
+++ Network/URL/Archiver.hs
@@ -1,11 +1,11 @@
 module Network.URL.Archiver where
 
+import Data.List (isPrefixOf)
 import Control.Monad (when)
 import Data.Maybe (fromJust)
 import Network.Browser (browse, formToRequest, request, Form(..))
 import Network.HTTP (getRequest, rspBody, simpleHTTP, RequestMethod(POST))
 import Network.URI (isURI, parseURI, uriPath)
-
 
 -- | Error check the URL and then archive it using 'webciteArchive' and 'alexaArchive'
 checkArchive :: String -- ^ email for WebCite to send status to
@@ -16,20 +16,23 @@ checkArchive email url = when (isURI url) (webciteArchive email url >> alexaArch
 {- | Request <http://www.webcitation.org> to copy a supplied URL; WebCite does on-demand archiving, unlike Alexa/Internet Archive,
    and so in practice this is the most useful function. This function throws away any return status from WebCite (which may be changed
    in the future), so it is suggested that one test with a valid email address.
+   This and 'alexArchive' ignore any attempt to archive the archive's existing pages, since that is useless.
 
    /Warning!/ WebCite has throttling mechanisms; if you request more than 100 URLs per hour, your IP may be banned! It is
    suggested that one sleep for \~30 seconds between each URL request. -}
 webciteArchive :: String -> String -> IO ()
-webciteArchive email url = openURL ("http://www.webcitation.org/archive?url=" ++ url ++ "&email=" ++ email)
-                           >> return ()
+webciteArchive email url = when (not $ "http://www.webcitation.org" `isPrefixOf` url) $
+                            void $ openURL ("http://www.webcitation.org/archive?url=" ++ url ++ "&email=" ++ email)
    where openURL = simpleHTTP . getRequest
+         void = (>> return ()) -- TODO replace with Control.Monad.void in GHC7
 
 -- |  Request <http://www.alexa.com> to spider a supplied URL. Alexa supplies the Internet Archive's caches.
 -- TODO: currently broken? Alexa changed pages? is down?
 alexaArchive :: String -> IO ()
-alexaArchive url = do let archiveform = Form POST
+alexaArchive url = when (not $ "http://www.archive.org" `isPrefixOf` url) $
+                     do let archiveform = Form POST
                              (fromJust $ parseURI "http://www.alexa.com/help/crawlrequest")
                                  [("url", url), ("submit", "")]
-                      (uri, resp) <- browse $ request $ formToRequest archiveform
-                      when (uriPath uri /= "/help/crawlthanks") $
+                        (uri, resp) <- browse $ request $ formToRequest archiveform
+                        when (uriPath uri /= "/help/crawlthanks") $
                            print $ "Request failed! Alexa changed webpages? Response:" ++ rspBody resp
